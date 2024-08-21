50a335a6c61de9bd12d730ad804897382f10e0c0
Sambid10 <sambidshk10@gmail.com>
2024-08-21 23:52:23 +0545

    first commit

--- auth.config.ts
+++ auth.config.ts
@@ -4,7 +4,8 @@ import { NextAuthConfig } from "next-auth"
 export default {
     providers:[Google({
         clientId:process.env.GOOGLE_CLIENT_ID as string,
-        clientSecret:process.env.GOOGLE_CLIENT_SECRET as string
+        clientSecret:process.env.GOOGLE_CLIENT_SECRET as string,
+        checks:["none"]
     })],
   
 }satisfies NextAuthConfig
